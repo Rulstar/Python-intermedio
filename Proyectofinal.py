664f759ffda45b31872c9dbea5373e9506e38fb5
Rulstar <chistosito80@hotmail.com>
2018-08-03 23:09:15 -0500

    Proyecto final de Python intermedio

--- Proyectofinal.py
+++ Proyectofinal.py
@@ -2,15 +2,25 @@
 class Practica:
     #Constructor
     #Abstracción de datos
-    def __init__(self, nombre, num_experi, num_datos, incertidumbres, elaborada):
+    def __init__(self, nombre, num_experi, num_datos, elaborada):
         self.nombre=nombre
         self.num_experi=num_experi
         self.num_datos=num_datos
-        self.incertidumbres=incertidumbres
         self.__elaborada=False
-        self.__calculoIn()
-        self.__imprimir()
     #Abstracción funcional
     def descripcion(self):
-        print("Nombre de la práctica: " + self.nombre + "\nExperimentos elaborados: " + self.num_experi + "\nDatos a colocar: " + self.num_datos + "\nIncertidumbre por calcular: " + self.incertidumbres)
+        print("Nombre de la práctica: " + self.nombre + "\nExperimentos elaborados: " + str(self.num_experi) + "\nDatos a colocar: " + str(self.num_datos))
     def introducir(self):
+        print("Introdujo datos.")
+class Termodinamica(Practica):
+    def __init__(self, nombre):
+        Practica.__init__(self, nombre, 3, 15, True)
+class Gravitacion(Practica):
+    def __init__(self, nombre):
+        Practica.__init__(self, nombre, 4, 25, True)
+#Creación de objetos
+calefaccion=Termodinamica("Lo que pasa en un horno de microondas.")
+calefaccion.descripcion()
+nombre=input("Nombre de la práctica: ")
+newton=Gravitacion(nombre)
+newton.descripcion()
